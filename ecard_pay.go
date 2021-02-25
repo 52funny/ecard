7ecd274c2b1928cbccad590feb4d3b1850776398
52funny <wwq9977@gmail.com>
2021-02-25 19:08:17 +0800

    add PayElectricity method

---
+++ ecard_pay.go
@@ -0,0 +1,58 @@
+package ecard
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/imroc/req"
+)
+
+// PayElectricity 支付电费
+func (e *Ecard) PayElectricity(areaNo string, buildingNo string, roomNo string, payMoney string) string {
+	resp, err := req.Post(e.URL+"/payFee/showItemListPayPage", req.Param{
+		"itemNum": "1",
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	// fmt.Println(resp.String())
+	reg := regexp.MustCompile("<input type=\"hidden\" id=\"itemNum\" value=\"(.*)\">")
+	itemNum := string(reg.FindSubmatch(resp.Bytes())[1])
+
+	reg = regexp.MustCompile("<input type=\"hidden\" id=\"typeNum\" value=\"(.*)\">")
+	typeNum := string(reg.FindSubmatch(resp.Bytes())[1])
+
+	reg = regexp.MustCompile("<input type=\"hidden\" id=\"itemName\" value=\"(.*)\">")
+	itemName := string(reg.FindSubmatch(resp.Bytes())[1])
+
+	reg = regexp.MustCompile("<li  onclick=\"compareItemMoney\\(1\\);\" balance=(.*) cardAccNum =\"(.*)\"  moneyMin =\"0\"  id=\"(.*)\">(.*)</li>")
+	wallet := reg.FindSubmatch(resp.Bytes())
+	eWalletMoney := string(wallet[1])
+	cardAccNum := string(wallet[2])
+	eWalletID := string(wallet[3])
+	eWalletName := string(wallet[4])
+
+	reg = regexp.MustCompile("<input type=\"hidden\" name=\"token\"  id =\"token\" value=\"(.*)\">")
+	token := string(reg.FindSubmatch(resp.Bytes())[1])
+
+	param := req.Param{
+		"itemNum":      itemNum,
+		"eWalletId":    eWalletID,
+		"typeNum":      typeNum,
+		"itemName":     itemName,
+		"payMenoy":     payMoney,
+		"ewalletMenoy": eWalletMoney,
+		"eWalletName":  eWalletName,
+		"areano":       areaNo,
+		"buildingno":   buildingNo,
+		"roomno":       roomNo,
+		// "buildingname": buildingNo + "号楼",
+		"cardaccNum": cardAccNum,
+		"token":      token,
+	}
+	resp, err = req.Post(e.URL+"/payFee/payItemlist", param)
+	reg = regexp.MustCompile("<p.*><strong>(.*)</strong></p.*>")
+	result := string(reg.FindSubmatch(resp.Bytes())[1])
+
+	return result
+}
