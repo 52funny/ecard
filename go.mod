b202707f86d1993d027b933c08f6458108aa6ff3
52funny <wwq9977@gmail.com>
2021-04-13 09:05:41 +0800

    add some testing

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/imroc/req v0.3.0
 	github.com/otiai10/gosseract v2.2.1+incompatible
 	github.com/otiai10/mint v1.3.2 // indirect
+	github.com/panjf2000/ants/v2 v2.4.3
 	github.com/tidwall/gjson v1.6.8
 	github.com/tidwall/pretty v1.1.0 // indirect
 	golang.org/x/net v0.0.0-20210224082022-3d97a244fca7 // indirect
---
+++ main_test.go
@@ -0,0 +1,195 @@
+package ecard
+
+import (
+	"fmt"
+	"log"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/panjf2000/ants/v2"
+)
+
+func TestRoutine(t *testing.T) {
+	fmt.Println(runtime.NumGoroutine())
+	needJobs := []string{}
+	lenOfJobs := len(needJobs)
+	fmt.Println(lenOfJobs)
+	time.Sleep(time.Second)
+	job := make(chan string, lenOfJobs)
+	result := make(chan struct{}, lenOfJobs)
+
+	e := getEcard()
+	e.MustLogin()
+	for i := 0; i < 30; i++ {
+		go func(id int, jobs chan string, results chan struct{}) {
+			for item := range jobs {
+				if ok, err := e.IsCookieOverDue(); err == nil {
+					if ok {
+						e.Login()
+					}
+				} else {
+					fmt.Println(err)
+					result <- struct{}{}
+					continue
+				}
+				fee, err := e.ObtainDormitoryElectricity("0", "5", item)
+				if err != nil {
+					log.Println(err)
+				}
+				fmt.Println(item, fee)
+				results <- struct{}{}
+			}
+		}(i, job, result)
+	}
+	for i := 0; i < len(needJobs); i++ {
+		job <- needJobs[i]
+	}
+	close(job)
+
+	for j := 0; j < len(needJobs); j++ {
+		<-result
+	}
+	fmt.Println(runtime.NumGoroutine())
+}
+
+func TestPool(t *testing.T) {
+	jobs := []string{
+		"237",
+		"k237",
+		"235",
+		"237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235", "237",
+		"k237",
+		"235",
+	}
+	e := getEcard()
+	e.MustLogin()
+	defer ants.Release()
+	var wait sync.WaitGroup
+	p, err := ants.NewPoolWithFunc(50, func(item interface{}) {
+		ans, _ := e.ObtainDormitoryElectricity("0", "5", item.(string))
+		fmt.Println(item, ans)
+		wait.Done()
+	})
+	if err != nil {
+		panic(err)
+	}
+	for _, item := range jobs {
+		wait.Add(1)
+		p.Invoke(item)
+	}
+	wait.Wait()
+}
