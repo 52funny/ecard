fe84c3a7a8a11f8d1d46d4cdf3e263a922b9a461
52funny <wwq9977@gmail.com>
2023-01-07 12:49:30 +0800

    Add golang native rsa encrypt method, not use js runtime.

--- utils/rsa.go
+++ utils/rsa.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"encoding/hex"
+	"math/big"
 	"sync"
 
 	"github.com/dop251/goja"
@@ -12,6 +14,24 @@ func RsaEncrypt(exponent string, modules string, str string) string {
 	var fn func(exponent string, modules string, str string) string
 	r.ExportTo(r.Get("encryptString"), &fn)
 	return fn(exponent, modules, str)
+}
+
+func RsaEncrypt2(exponent string, modules string, str string) string {
+	modulus := new(big.Int)
+	modulus.SetString(modules, 16)
+	exp := new(big.Int)
+	exp.SetString(exponent, 16)
+	decimals := []byte(str)
+	result := new(big.Int)
+	for i, d := range decimals {
+		acc := new(big.Int)
+		acc.SetInt64(int64(d))
+		acc.Lsh(acc, uint(i*8))
+		result.Add(result, acc)
+	}
+	result.Exp(result, exp, modulus)
+
+	return hex.EncodeToString(result.Bytes())
 }
 
 // ***it not work now***
